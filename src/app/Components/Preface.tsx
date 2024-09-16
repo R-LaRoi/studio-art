edb0a66c11233fae68872bff4a4604381d479530
R. LaRoi <are@raewhoo.com>
2024-09-16 19:20:08 -0400

    Added Components
    image
    nav

---
+++ src/app/Components/Preface.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import relfig from '../assets/relfig.jpg'
+
+type Props = {}
+
+export default function Preface({ }: Props) {
+  return (
+    <>
+      <div className="h-screen flex flex-col md:flex-row">
+        <div className="w-full bg-white md:w-1/2 p-4 overflow-y-auto">
+          <Image
+            src={relfig}
+            alt="charcoal drawing"
+            className="w-1/2"
+          />
+        </div>
+
+        <div className=" w-full md:w-1/2 bg-white p-4 overflow-y-auto h-screen flex items-end justify-end">
+          <ul className="text-6xl space-y-5 ">
+            <li className="flex justify-between w-full ">guardians <span className='pl-3'>01</span></li>
+            <li className="flex justify-between w-full">thirty-thousand-five-sixty-two <span className='pl-3'>02</span></li>
+            <li className="flex justify-between w-full">flèche faîtière<span className='pl-3'>03</span></li>
+            <li className="flex justify-between w-full">paintings<span className='pl-3'>04</span></li>
+            <li className="flex justify-between w-full">contact<span className='pl-3'>00</span></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-
+import Preface from "./Components/Preface";
 import Intro from "./Pages/Intro";
 
 export default function Home() {
   return (
     <>
       <Intro />
+      <Preface />
 
     </>
   );
