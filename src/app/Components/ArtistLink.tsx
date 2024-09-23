f7952ba2b5c856b6ad9ab90c55dcf8c4c8d9401e
R. LaRoi <are@raewhoo.com>
2024-09-23 12:33:27 -0400

    Nav

--- src/app/Components/ArtistLink.tsx
+++ src/app/Components/ArtistLink.tsx
@@ -1,13 +1,91 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { navLinks } from '../assets/allContent'
+import { Spin as Hamburger } from 'hamburger-react'
+import { motion } from "framer-motion";
+
+
+
+const menu = {
+  open: { transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] } },
+  closed: {
+    top: "0px",
+    right: "0px",
+    transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1] }
+
+  }
+
+}
 
 
 
 export default function ArtistLink() {
-  return (
-    <Link href="/">
-      <div className='fixed top-0 left-0 text-4xl p-4 bg-transparent font-bold tracking-wide transition duration-300 hover:bg-black hover:text-white hover:rounded-sm'>rachelstroy</div>
-    </Link>
 
+  const [navActive, setNavActive] = useState(false);
+  const showNavMenu = () => {
+    setNavActive(!navActive)
+  }
+
+  return (
+    <>
+
+      {navActive ?
+
+        <div className='fixed inset-0 h-screen w-screen bg-[#0C0A09] z-[100] text-center flex items-center justify-center'>
+
+          <motion.div
+            variants={menu}
+            animate={navActive ? "open" : "closed"}
+            initial="closed">
+            <div onClick={showNavMenu} className="fixed top-4 left-4 rounded-full cursor-pointer inline-flex items-center justify-center bg-[#eaeaea] transition-colors duration-500 hover:bg-gray-700 ">
+              <Hamburger
+
+                toggled={navActive} toggle={setNavActive} />
+            </div>
+            <div className="">
+              <ul className="w-full max-w-md px-4">
+                {navLinks.map((link, index) => (
+                  <Link key={index} href={link.href}>
+                    <li className="flex justify-between w-full mb-6 cursor-pointer text-white hover:text-gray-700 border-b-2 border-transparent hover:border-gray-700 transition">
+                      <span className="truncate mr-2">{link.text}</span>
+                      <span className="pl-2">{link.number}</span>
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+
+          </motion.div>
+
+        </div>
+
+
+        :
+
+
+        <div onClick={showNavMenu} >
+
+          {/* <Link href="/"> */}
+          <div className='fixed top-0 left-0 text-4xl p-4 bg-transparent font-bold tracking-wide transition duration-300 hover:bg-black hover:text-white hover:rounded-sm' >rachelstroy</div>
+          {/* </Link> */}
+
+
+
+        </div>
+
+
+
+
+      }
+
+
+
+
+
+
+
+    </>
   )
 }
