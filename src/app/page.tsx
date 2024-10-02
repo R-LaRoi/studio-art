c79d8008208eaa3e8866f5771fc4807c1268beff
R. LaRoi <are@raewhoo.com>
2024-10-02 10:02:42 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 
 import Intro from "./Components/Intro";
-import Nametag from "./Components/Nametag";
+
 
 export default function Home() {
   return (
 
-    <><Intro />
+    <>
+      <Intro />
     </>
   );
 }
